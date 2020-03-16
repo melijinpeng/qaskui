36d5eedf15bbf1a6628e0f34c6dc8f409c65e795
lijinpeng <melijinpeng@163.com>
2020-03-16 20:29:05 +0800

    重置了项目

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# kwui
+# qaskui
 
 ## Project setup
 ```
--- examples/App.vue
+++ examples/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <kw-button>button</kw-button>
+      <kw-table></kw-table>
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
     </div>
